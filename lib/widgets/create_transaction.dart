d460fb7c4a393d79f70cbcef09b2bb4856c42b8d
Anna Maria Fortuna <amvfortuna@gmail.com>
2021-01-19 19:59:33 +1300

    Added uuid package to generate UUIDs for each Transaction.

--- lib/widgets/create_transaction.dart
+++ lib/widgets/create_transaction.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 import '../models/transaction.dart';
 import './transaction_form.dart';
@@ -11,21 +11,24 @@ class CreateTransaction extends StatefulWidget {
 }
 
 class CreateTransactionState extends State<CreateTransaction> {
-  List<Transaction> _transactions = [
+  final _transactions = [
     Transaction(
+      id: Uuid().v4(),
       name: 'Cheesy feet rubber cheese mascarpone.',
       amount: 500,
       date: DateTime.now(),
     ),
     Transaction(
+      id: Uuid().v4(),
       name: 'Sample 2',
       amount: 1000,
       date: DateTime.now(),
     ),
   ];
 
-  void addTransaction(String title, double amount) {
-    Transaction transaction = Transaction(
+  void _addTransaction(String title, double amount) {
+    final transaction = Transaction(
+      id: Uuid().v1(),
       name: title,
       amount: amount,
       date: DateTime.now(),
@@ -41,7 +44,7 @@ class CreateTransactionState extends State<CreateTransaction> {
     return Column(
       children: [
         TransactionForm(
-          onAddButtonTapped: addTransaction,
+          onAddButtonTapped: _addTransaction,
         ),
         TransactionList(
           transactions: _transactions,
